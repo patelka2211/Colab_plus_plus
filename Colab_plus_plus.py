c559558da184db8c3658e17ea7c5cffc36ec3ecf
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2022-09-15 23:58:02 +0530

    First commit.

---
+++ Colab_plus_plus.py
@@ -0,0 +1,53 @@
+'''
+MIT License
+
+Copyright (c) 2022 Kartavya Patel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
+class Colab_plus_plus:
+    def __init__(self, file_name):
+        self.file_name = file_name.split('.')
+        self.file_extension = self.file_name[-1]
+        self.bash_file = f'{".".join(self.file_name[0:-1])}_{self.file_extension}.sh'
+        self.file_name = '.'.join(self.file_name)
+
+    def source_code(self, source):
+        if self.file_extension in ['js']:
+            with open(self.file_name, 'w') as file:
+                file.write(source)
+
+            with open(self.bash_file, 'w') as file:
+                file.write(f'node {self.file_name}')
+
+        elif self.file_extension in ['cpp', 'c++']:
+            with open(self.file_name, 'w') as file:
+                file.write(source)
+
+            with open(self.bash_file, 'w') as file:
+                file.write(
+                    f'g++ {self.file_name} -o cpp_output_file && ./cpp_output_file')
+        else:
+            self.bash_file = 'error.sh'
+            with open(self.bash_file, 'w') as file:
+                file.write(f'''echo "\'.{self.file_extension}\' is not supported file extensions by \"Colab_plus_plus\" as of now."
+                echo "See supported file extensions at https://github.com/patelka2211/Colab_plus_plus#supported-languages"
+                echo "or let me know what language you want to use, we will try to make it work if possible 🙌🏻. Just make a new issue at https://github.com/patelka2211/Colab_plus_plus/issues/new"''')
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Colab_plus_plus
- Run programs written in languages other than python in Colab.
+# Colab plus plus
+
+Run programs written in languages other than Python in Colab.
+
+![](https://opengraph.githubassets.com/1/patelka2211/Colab_plus_plus)
+
+## ⚙️ Setup
+
+To setup Colab_plus_plus in your Jupyter Notebook first run this command in first cell.
+
+```bash
+# Setting up Colab_plus_plus environment
+!curl https://raw.githubusercontent.com/patelka2211/Colab_plus_plus/main/Colab_plus_plus.py --output Colab_plus_plus.py
+
+from Colab_plus_plus import Colab_plus_plus
+```
+
+## 🔥 Usage
+
+Suppose you want to use Colab for C++ the below code is what you need.
+
+```py
+# Running C++ in Colab
+cpp_file_obj = Colab_plus_plus('main.cpp')
+
+cpp_file_obj.source_code('''
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world"<<endl;
+    return 0;
+}
+''')
+
+!sh $cpp_file_obj.bash_file
+```
+
+```py
+# Running JavaScript in Colab
+js_file_obj = Colab_plus_plus('main.js')
+
+js_file_obj.source_code('''
+console.log('Hello world');
+''')
+
+!sh $js_file_obj.bash_file
+```
+
+`Also see which extensions you can use with which language.` click [here](./alternative-extensions.json) to know more.
+
+## ⚡️ Example
+
+Here is the example on [Colab](https://colab.research.google.com/drive/1GJnRiw_JkWBGbat7VjCzEAkJkSENBNLO?usp=sharing "Go to example").
+
+## ✅ Supported languages
+
+|  Language  | Support |
+| :--------: | :-----: |
+|    C++     |   ✅    |
+| JavaScript |   ✅    |
+
+## 📝 License
+
+Copyright (c) [Kartavya Patel](https://github.com/patelka2211 "Kartavya Patel on Github"). All rights reserved.
+
+Licensed under the [MIT](./LICENSE "See license") license.
